7bc3c63b652c4abd0066b24ce9cb1fb831038c8d
pratarora <pratarora@gmail.com>
2018-08-07 14:34:28 +0530

    topic modelling start

--- search analysis.R
+++ search analysis.R
@@ -4,8 +4,13 @@ setwd("E:/Data Science/Google data Prateek January 2018/google data/Analysis/Sea
 
 #libraries-----------
 library(jsonlite)
-library(tidyjson)
-library(tidyverse)
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+# library(broom)
+library(tibble)
+library(purrr)
+library(readr)
 library(httr)
 library(lubridate)
 library(scales)
@@ -16,6 +21,8 @@ library(ColorPalette)
 library(RColorBrewer)
 library(wordcloud)
 library(wordcloud2)
+library(topicmodels)
+
 set.seed(2008)
 
 
@@ -61,7 +68,7 @@ data_timechanged <- data %>%
 
 data_timechanged$Weekday <- factor(data_timechanged$Weekday, levels= c("Sunday", "Monday", 
                                          "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"))
-rm(data)
+# rm(data)
 
 #allcounts and merging in main dataframe----
 yearlystats <- data_timechanged %>%  group_by(Year) %>% summarise(yearlycount= n())
@@ -85,7 +92,7 @@ data_timechanged <-  merge(data_timechanged,dailystats)
 hourlystats <- data_timechanged %>%  group_by(Hour) %>% summarise(hourlycount= n()) 
 data_timechanged <-  merge(data_timechanged,hourlystats)
 
-rm(list=setdiff(ls(), "data_timechanged"))
+# rm(list=setdiff(ls(), "data_timechanged"))
 
 
 
@@ -297,7 +304,7 @@ data_filtered_r <- data_timechanged %>% filter(str_detect(search_query,paste(rte
 
 termstoremove_R <- c("rajdhani","rupee","rome","room","research","rej","reg","rail","rys","reddit","ric")
 
-data_filtered_r <- filter(data_filtered_r,!str_detect(search_query,paste(termstoremove,collapse="|")))
+data_filtered_r <- filter(data_filtered_r,!str_detect(search_query,paste(termstoremove_R,collapse="|")))
 
 aa <- ggplot(data= data_filtered_r, aes(x=Week))+
   geom_bar(stat="count",fill="blue" ) +
@@ -308,3 +315,44 @@ aa <- ggplot(data= data_filtered_r, aes(x=Week))+
 print(aa)
 
 
+top_terms_by_topic_LDA <- function(input_text, # should be a columm from a dataframe
+                                   plot = T, # return a plot? TRUE by defult
+                                   number_of_topics = 4) # number of topics (4 by default)
+{
+  # create a corpus (type of object expected by tm) and document term matrix
+  Corpus <- Corpus(VectorSource(input_text)) # make a corpus object
+  DTM <- DocumentTermMatrix(Corpus) # get the count of words/document
+  
+  # remove any empty rows in our document term matrix (if there are any 
+  # we'll get an error when we try to run our LDA)
+  unique_indexes <- unique(DTM$i) # get the index of each unique value
+  DTM <- DTM[unique_indexes,] # get a subset of only those indexes
+  
+  # preform LDA & get the words/topic in a tidy text format
+  lda <- LDA(DTM, k = number_of_topics, control = list(seed = 1234))
+  topics <- tidy(lda, matrix = "beta")
+  
+  # get the top ten terms for each topic
+  top_terms <- topics  %>% # take the topics data frame and..
+    group_by(topic) %>% # treat each topic as a different group
+    top_n(10, beta) %>% # get the top 10 most informative words
+    ungroup() %>% # ungroup
+    arrange(topic, -beta) # arrange words in descending informativeness
+  
+  # if the user asks for a plot (TRUE by default)
+  if(plot == T){
+    # plot the top ten terms for each topic in order
+    top_terms %>% # take the top terms
+      mutate(term = reorder(term, beta)) %>% # sort terms by beta value 
+      ggplot(aes(term, beta, fill = factor(topic))) + # plot beta by theme
+      geom_col(show.legend = FALSE) + # as a bar plot
+      facet_wrap(~ topic, scales = "free") + # which each topic in a seperate plot
+      labs(x = NULL, y = "Beta") + # no x label, change y label 
+      coord_flip() # turn bars sideways
+  }else{ 
+    # if the user does not request a plot
+    # return a list of sorted terms instead
+    return(top_terms)
+  }
+}
+top_terms_by_topic_LDA(data$event.query.query_text, number_of_topics = 2)
